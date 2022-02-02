dfbbb83137258239bd16c7cb21efd065e98bf3b1
IsaacMvmv <67015742+IsaacMvmv@users.noreply.github.com>
2022-02-02 19:02:32 +0100

    Update uninstall-doom3.sh

--- scripts/doom3/uninstall-doom3.sh
+++ scripts/doom3/uninstall-doom3.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sudo rm -rf /bin/doom3 ~/.games/doom3 ~/.local/share/dhewm3/ ~/.local/share/applications/Doom3.desktop
+sudo rm -rf /bin/doom3 ~/.games/doom3 ~/.local/share/dhewm3/ ~/.config/dhewm3 ~/.local/share/applications/Doom3.desktop
