41c15579d33c99afed8790083ffb527cef46ece3
IsaacMvmv <67015742+IsaacMvmv@users.noreply.github.com>
2022-02-08 16:00:47 +0100

    Update install-acr.sh

--- scripts/AssaultCubeReloaded/install-acr.sh
+++ scripts/AssaultCubeReloaded/install-acr.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-if [ -f /bin/apt ];then
+if [ -f /usr/bin/apt ];then
 	sudo apt install -y gcc g++ make git libsdl1.2debian-dev libsdl-image1.2-dev zlib1g-dev libogg0-dev libvorbis0a-dev libopenal1-dev libcurl4-dev libsdl1.2debian libsdl-image1.2 zlib1g libogg0 libvorbis0a libopenal1 libcurl4 
-elif [ -f /bin/pacman ];then
+elif [ -f /usr/bin/pacman ];then
 	sudo pacman -S --needed gcc make sdl openal git libvorbis libogg zlib sdl_image sdl libx11
 fi
 
