191cb76be1bb270a933975309086bbb03867d85e
IsaacMvmv <67015742+IsaacMvmv@users.noreply.github.com>
2022-02-27 16:40:02 +0100

    Update install-mangohud.sh

--- scripts/mangohud/install-mangohud.sh
+++ scripts/mangohud/install-mangohud.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ -f /usr/bin/apt ];then
-	sudo apt install -y git ninja-build python3-mako gcc g++ meson python3 libxnvctrl-dev libdbus-1-dev libx11-dev libdrm-dev mesa-common-dev glslang-dev glslang-tools
+	sudo apt install -y git ninja-build python3-pip gcc g++ meson python3 libxnvctrl-dev libdbus-1-dev libx11-dev libdrm-dev mesa-common-dev glslang-dev glslang-tools
 	python3 -m pip install mako
 elif [ -f /usr/bin/pacman ];then
 	sudo pacman -S --needed git ninja gcc meson python-mako dbus libdrm glslang python3-mako
