4d88ba249e6498a461e13a6aaa86099cdccb07fc
IsaacMvmv <67015742+IsaacMvmv@users.noreply.github.com>
2022-04-13 11:03:29 +0200

    Update uninstall-doom.sh

--- scripts/doom12/uninstall-doom.sh
+++ scripts/doom12/uninstall-doom.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-sudo rm -rf /bin/doom1 /bin/doom2 ~/.games/doom1_2 ~/.local/share/applications/Doom2.desktop ~/.local/share/applications/Doom1.desktop ~/.local/share/prboom-plus
+sudo rm -rf /bin/doom1 /bin/doom2 ~/.games/doom1_2 ~/.local/share/applications/Doom2.desktop ~/.local/share/applications/Doom1.desktop ~/.local/share/prboom-plus 
+
+
+if [ -f /usr/bin/pacman ];then
+  sudo pacman -Rs freepats-legacy
+  sudo rm -rf /etc/timidity++/freepats.cfg
+fi
